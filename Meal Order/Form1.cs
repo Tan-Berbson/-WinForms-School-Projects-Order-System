62a00260d9241171730a0efa5722913999187e0f
ACT-STUDENT <ACT-STUDENT@LAB4-PC12>
2026-02-04 15:15:42 +0800

    Add project files.

---
+++ Meal Order/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Meal_Order
+{
+    public partial class OrderForm : Form
+    {
+        public OrderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            
+            if (cbm_Order.SelectedItem == null)
+            {
+                MessageBox.Show("Please select an order before confirming.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string order = cbm_Order.SelectedItem.ToString();
+
+            string checkbox = "";
+            if (chk_Friess.Checked )
+            {
+                checkbox += "," + "Friess ,";
+            }
+            if( chk_Gravy.Checked )
+            {
+                checkbox += "," + "Gravy ,";
+            }
+            if (chk_sundae.Checked )
+            {
+                checkbox += "," + "Sundae";
+            }
+            ListViewItem ordersum = new ListViewItem(order);
+            ordersum.SubItems.Add(checkbox);
+            listview_OrderSum.Items.Add(ordersum);
+           
+
+
+        }
+
+        private void OrderForm_Load(object sender, EventArgs e)
+        {
+            listview_OrderSum.Columns.Add("Order", 120);
+            listview_OrderSum.Columns.Add("Adds on", 120);
+        }
+        private void clear()
+        {
+            cbm_Order.SelectedItem = null;
+            chk_Friess.Checked = false;
+            chk_Gravy.Checked = false;
+            chk_sundae.Checked = false;
+        }
+
+        private void btn_Confirm_Click(object sender, EventArgs e)
+        {
+            if ( cbm_Order.SelectedItem == null)
+            {
+                MessageBox.Show("Please select an order before confirming.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            
+            Form2 f = new Form2();
+            foreach(ListViewItem ord in listview_OrderSum.Items)
+            {
+                string order = ord.Text;
+                string addons = ord.SubItems[1].Text;
+                f.listBox1.Items.Add(order);
+                f.listBox1.Items.Add("Adds - on" + addons);
+
+            }
+            f.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Meal Order/Form2.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Meal_Order
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.btn_Ordernow = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rb_DineIn = new System.Windows.Forms.RadioButton();
+            this.rb_takeout = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(8, 57);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(379, 225);
+            this.listBox1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(1, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(407, 546);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(23, 21);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(350, 60);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.panel6);
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Controls.Add(this.listBox1);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Location = new System.Drawing.Point(3, 105);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(401, 438);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Red;
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(404, 51);
+            this.panel4.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.btn_Ordernow);
+            this.panel5.Location = new System.Drawing.Point(0, 384);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(404, 51);
+            this.panel5.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(11, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.White;
+            this.panel6.Controls.Add(this.rb_takeout);
+            this.panel6.Controls.Add(this.rb_DineIn);
+            this.panel6.Controls.Add(this.panel7);
+            this.panel6.Location = new System.Drawing.Point(-3, 288);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(407, 100);
+            this.panel6.TabIndex = 2;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Red;
+            this.panel7.Controls.Add(this.label1);
+            this.panel7.Location = new System.Drawing.Point(0, 0);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(407, 40);
+            this.panel7.TabIndex = 0;
+            // 
+            // btn_Ordernow
+            // 
+            this.btn_Ordernow.BackColor = System.Drawing.Color.Red;
+            this.btn_Ordernow.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Ordernow.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_Ordernow.Location = new System.Drawing.Point(3, 8);
+            this.btn_Ordernow.Name = "btn_Ordernow";
+            this.btn_Ordernow.Size = new System.Drawing.Size(398, 38);
+            this.btn_Ordernow.TabIndex = 0;
+            this.btn_Ordernow.Text = "Order Now";
+            this.btn_Ordernow.UseVisualStyleBackColor = false;
+            this.btn_Ordernow.Click += new System.EventHandler(this.btn_Ordernow_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(11, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dine Type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(8, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Order Confirmation";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(132, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 33);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Jollibee";
+            // 
+            // rb_DineIn
+            // 
+            this.rb_DineIn.AutoSize = true;
+            this.rb_DineIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_DineIn.Location = new System.Drawing.Point(84, 56);
+            this.rb_DineIn.Name = "rb_DineIn";
+            this.rb_DineIn.Size = new System.Drawing.Size(85, 24);
+            this.rb_DineIn.TabIndex = 2;
+            this.rb_DineIn.TabStop = true;
+            this.rb_DineIn.Text = "Dine In";
+            this.rb_DineIn.UseVisualStyleBackColor = true;
+            // 
+            // rb_takeout
+            // 
+            this.rb_takeout.AutoSize = true;
+            this.rb_takeout.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_takeout.Location = new System.Drawing.Point(216, 56);
+            this.rb_takeout.Name = "rb_takeout";
+            this.rb_takeout.Size = new System.Drawing.Size(100, 24);
+            this.rb_takeout.TabIndex = 3;
+            this.rb_takeout.TabStop = true;
+            this.rb_takeout.Text = "Take Out";
+            this.rb_takeout.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 561);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Ordernow;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rb_DineIn;
+        private System.Windows.Forms.RadioButton rb_takeout;
+    }
+}
